2a891a0e83e1bc81f03051651e027116aea365b6
Emily Chen <chenemily3@yahoo.com>
2025-02-22 20:06:07 -0600

    Allow basic placement of objects

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,10 +1,29 @@
 using UnityEngine;
+using System;
+using UnityEngine.EventSystems;
 
 public class InputManager : MonoBehaviour
 {
     [SerializeField] private Camera mainCam;
     private Vector3 lastPosition;
     [SerializeField] private LayerMask placementLayerMask;
+
+    public event Action OnClicked, OnExit;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            OnClicked?.Invoke();
+        }
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            OnExit?.Invoke();
+        }
+    }
+
+    // TODO: replace this with something that works
+    public bool IsPointerOverUI() => EventSystem.current.IsPointerOverGameObject();
 
     public Vector3 GetSelectedMapPosition()
     {
